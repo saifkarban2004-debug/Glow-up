1a6e91ea47d04e10537879c4eb11dc84bd11bc49
saif ali <saifkarban2004@.com>
2026-07-31 21:06:21 +0200

    fix: whitelist google avatar hostname in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "via.placeholder.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
